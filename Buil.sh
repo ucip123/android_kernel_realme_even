1faea9faab2eaf8a4b4af480e8426d61d1641d2b
ucip123 <98519520+ucip123@users.noreply.github.com>
2023-09-18 00:30:09 -0700

    Update Buil.sh

--- Buil.sh
+++ Buil.sh
@@ -9,5 +9,13 @@ export LOCALVERSION="KernelAbalAbal"
 make O=out ARCH=arm64 k68v1_64_k419_defconfig
 
 PATH="/workspace/zyc/bin:$PATH" \
-make -j$(nproc --all) O=out       ARCH=arm64       LD=ld.lld       NM=llvm-nm       AR=llvm-ar       CC="ccache clang"       CLANG_TRIPLE=aarch64-linux-gnu-      CROSS_COMPILE=aarch64-linux-gnu-       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-V=0 2>&1 | tee log.txt
+make -j$(nproc --all) O=out \
+      ARCH=arm64 \
+      LD=ld.lld \
+      NM=llvm-nm \
+      AR=llvm-ar \
+      CC="ccache clang" \
+      CLANG_TRIPLE=aarch64-linux-gnu- \
+      CROSS_COMPILE=aarch64-linux-gnu- \
+      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+      V=0 2>&1 | tee log.txt
